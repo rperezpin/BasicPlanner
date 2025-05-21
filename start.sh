51930477194e5cc1d4368a2288ff87b6ef254447
rperezpin <rubenpi1996@gmail.com>
2025-05-21 20:44:23 +0200

    Cambio a railway

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-set -e
-echo "Starting Odoo on port $PORT..."
-/usr/bin/odoo -c /etc/odoo/odoo17.conf --http-port=$PORT --log-level=debug --xmlrpc-interface=0.0.0.0
+
+export PGHOST=${PGHOST}
+export PGPORT=${PGPORT}
+export PGUSER=${PGUSER}
+export PGPASSWORD=${PGPASSWORD}
+export PGDATABASE=${PGDATABASE}
+
+/usr/bin/odoo -c /etc/odoo/odoo17.conf --http-port=$PORT --xmlrpc-interface=0.0.0.0
